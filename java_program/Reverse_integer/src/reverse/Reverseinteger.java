55a09cc513c6fbda64d3db729e0a5b9b913b55c4
dashrathh043 <dashrath043soni@gmail.com>
2021-09-12 23:39:22 +0530

    chnages

--- java_program/Reverse_integer/src/reverse/Reverseinteger.java
+++ java_program/Reverse_integer/src/reverse/Reverseinteger.java
@@ -1,9 +1,32 @@
 package reverse;
 
 public class Reverseinteger {
+	    public int reverse(int x) {
+	       boolean bool = false;
+	        long rev_num = 0;
+	        if(x<0){
+	            bool = true;
+	            x = Math.abs(x);
+	        }
+	        
+	        while(x>0){
+	            int rem = x%10;
+	            rev_num = (rev_num*10)+rem;
+	            x = x/10;
+	        }
+	        
+	        if(bool == true)
+	            rev_num = -1*rev_num;
+	        
+	        if(rev_num>=Integer.MAX_VALUE||rev_num<=Integer.MIN_VALUE)
+	            return 0;
+	        return (int)rev_num;
+	        
+	    }
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Reverseinteger obj = new Reverseinteger();
+		System.out.println(obj.reverse(1534236469));
 	}
 
 }
